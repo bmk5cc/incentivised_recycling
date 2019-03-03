e6719d42207c32b2b3fe5299bc3559558bc8ffd4
Very-Red <bmk5cc@virginia.edu>
2019-03-03 02:51:28 -0500

    centered main page

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -55,7 +55,9 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text('My Flutter App'),
       ),
-      body: _children[_currentIndex], // new
+      body: Center(
+          child: _children[_currentIndex]
+      ), // new
       bottomNavigationBar: BottomNavigationBar(
         onTap: onTabTapped, // new
         currentIndex: _currentIndex, // new
